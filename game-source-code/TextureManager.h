626e98c861c4717098c4009e6dc4654fc959836a
mikaeeld <41543031+Mikaeeld@users.noreply.github.com>
2021-09-30 13:29:36 +0200

    Add basic texture manager concept

---
+++ game-source-code/TextureManager.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "ResourceManager.h"
+#include "SFML/Graphics.hpp"
+
+class TextureManager : public ResourceManager<TextureManager, sf::Texture>
+{
+public:
+    TextureManager() : ResourceManager("textures.cfg") {}
+
+    sf::Texture *load(const string *path)
+    {
+        sf::Texture *texture = new sf::Texture();
+        if (!texture->loadFromFile(*path))
+        {
+            delete texture;
+            texture = nullptr;
+            cerr << "failed to load texture";
+        }
+        return texture;
+    }
+};
