0a0100fc0ce358815d588cb05124728cb24048c6
mikaeeld <41543031+Mikaeeld@users.noreply.github.com>
2021-10-01 03:10:46 +0200

    Added function to get resource location

---
+++ game-source-code/ResourcePath.cpp
@@ -0,0 +1,18 @@
+#include "ResourcePath.h"
+
+std::string resourcePath()
+{
+#if defined(__APPLE__)
+    auto mainBundle = CFBundleGetMainBundle();
+    auto resourcesURL = CFBundleCopyResourcesDirectoryURL(mainBundle);
+    char path[1024];
+    if (!CFURLGetFileSystemRepresentation(resourcesURL, TRUE, (UInt8 *)path, 1024))
+    {
+        throw std::runtime_error("Cannot get Resource Path");
+    }
+    CFRelease(resourcesURL);
+    return strncat(path, "/", 1024);
+#else
+    return "";
+#endif
+}
---
+++ game-source-code/ResourcePath.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+#if defined(__APPLE__)
+#include <CoreFoundation/CoreFoundation.h>
+#endif
+
+std::string resourcePath();
