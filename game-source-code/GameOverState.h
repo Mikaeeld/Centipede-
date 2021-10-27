9d669900dfa775680113494d97c12ded877f0c39
shivun-wits <80011093+shivun-wits@users.noreply.github.com>
2021-10-27 20:58:10 +0200

    added: Doxygen comments for new and updated states

--- game-source-code/GameOverState.h
+++ game-source-code/GameOverState.h
@@ -3,13 +3,24 @@
 #include "ResourcePath.h"
 #include "SFML/Graphics.hpp"
 
+/**
+ * @brief This state is present when the game has completed
+ *
+ * The player can press enter to restart the game, or escape to quit.
+ *
+ */
 class GameOverState : public State
 {
 public:
+	/**
+	 * @brief Construct a new Game Over State object
+	 *
+	 * @param windowSize  A vector containing the window size
+	 */
 	GameOverState(const sf::Vector2f &windowSize);
 
+	// Re-implemented functions
 	const vector<Drawable_ptr> getDrawable();
-
 	void handleInput();
 
 private:
--- game-source-code/State.h
+++ game-source-code/State.h
@@ -30,21 +30,90 @@ public:
 	 */
 	virtual const vector<Drawable_ptr> getDrawable();
 
+	/**
+	 * @brief Construct a new State object
+	 *
+	 */
 	State();
 
+	/**
+	 * @brief Handles user input
+	 *
+	 * To be defined when inherited, otherwise does nothing by default
+	 *
+	 */
 	virtual void handleInput();
 
+	/**
+	 * @brief Resets the state of important variables: nextSate_ & exit_
+	 *
+	 */
 	virtual void reset();
 
+	/**
+	 * @brief Returns the value of the 'renderWithAbove' flag
+	 *
+	 * If true the state requests to be rendered under the currently active state
+	 * @return true
+	 * @return false
+	 */
 	const bool &renderWithAbove() const;
+
+	/**
+	 * @brief Returns the value of the 'renderBelow' flag
+	 *
+	 * If true the state allows for the state beneath to be rendered with it
+	 * @return true
+	 * @return false
+	 */
 	const bool &renderBelow() const;
+
+	/**
+	 * @brief Returns the value of the 'nextState' flag
+	 *
+	 * If true the state requests the next state becomes active instead
+	 * @return true
+	 * @return false
+	 */
 	const bool &nextState() const;
+
+	/**
+	 * @brief Returns the value of the 'exit' flag
+	 *
+	 * If true the state requests that the window / application be terminated
+	 * @return true
+	 * @return false
+	 */
 	const bool &exit() const;
 
 protected:
+	/**
+	 * @brief An entity manager belnging to the state
+	 *
+	 */
 	EntityManager entityManager_;
+
+	/**
+	 * @brief If true the state requests to be rendered under the currently active state
+	 *
+	 */
 	bool renderWithAbove_;
+
+	/**
+	 * @brief  If true the state allows for the state beneath to be rendered with it
+	 *
+	 */
 	bool renderBelow_;
+
+	/**
+	 * @brief If true the state requests the next state becomes active instead
+	 *
+	 */
 	bool nextState_;
+
+	/**
+	 * @brief If true the state requests that the window / application be terminated
+	 *
+	 */
 	bool exit_;
 };
