feb2128fb4671b64acad00fce2603647368dc69f
mikaeeld <41543031+Mikaeeld@users.noreply.github.com>
2021-10-27 16:47:35 +0200

    Comments for spider, state and ship

--- game-source-code/Ship.h
+++ game-source-code/Ship.h
@@ -5,18 +5,31 @@
 
 using Texture_ptr = shared_ptr<sf::Texture>;
 
+/**
+ * @brief Ship Class
+ * 
+ * Ship (Player) entity
+ * 
+ */
 class Ship : public GameEntity
 {
 public:
+	/**
+	 * @brief Construct a new Ship object
+	 * 
+	 */
 	Ship();
 
-	~Ship() {}
-
+	//Ship movement bounds
 	constexpr static const float MAX_X = 240.0f - 4.0f; // 256 - 8
 	constexpr static const float MAX_Y = 248.0f - 4.0f;
 	constexpr static const float MIN_X = 3.0f;
 	constexpr static const float MIN_Y = 200.0f + 4.0f;
 
+	/**
+	 * @brief Possible ship movement directions
+	 * 
+	 */
 	enum class Direction
 	{
 		Up,
@@ -25,25 +38,59 @@ public:
 		Right
 	};
 
+	/**
+	 * @brief The state of the player
+	 * 
+	 */
 	enum class Condition
 	{
 		Alive,
 		Dying
 	};
 
+	/**
+	 * @brief Moves the ship in the direction specified
+	 * 
+	 * @param direction Direction of movement
+	 * @param time Tick time used to keep speed fixed
+	 */
 	void inputMove(Direction direction, sf::Time time);
+
+	//Re-implemented funcitons
 	void tick(const sf::Time &time);
 	bool handleCollision(entityType type, sf::FloatRect collisionRect, const shared_ptr<GameEntity> other);
+	entityType getType();
+	
+	/**
+	 * @brief Fire a bullet from the ship and animate accordingly
+	 * 
+	 * Also ensures firing is rate limited
+	 * 
+	 */
 	void fire();
+
+	/**
+	 * @brief Trigger the ship's explosion animation and enter the dying state
+	 * 
+	 */
 	void explode();
+
+	/**
+	 * @brief Return the ship sprite to it's normal (non-firing) state
+	 * 
+	 */
 	void normal();
 
+	/**
+	 * @brief Get the Condition of the ship
+	 * 
+	 * @return const Condition& 
+	 */
 	const Condition &getCondition()
 	{
 		return condition_;
 	}
 
-	entityType getType();
 
 private:
 	void handleInput(sf::Time time);
--- game-source-code/State.h
+++ game-source-code/State.h
@@ -5,15 +5,33 @@
 
 using Drawable_ptr = shared_ptr<sf::Drawable>;
 
+/**
+ * @brief Base State class
+ * 
+ * The base for gameplay states
+ * Contains entities of state and can be updated as required
+ * 
+ */
 class State
 {
 
 public:
+
+	/**
+	 * @brief Updates the state and entities contained within
+	 * 
+	 * @param time 
+	 */
 	virtual void update(const sf::Time &time)
 	{
 		(void)time;
 	};
 
+	/**
+	 * @brief Get all drawable objects contained in the state
+	 * 
+	 * @return const vector<Drawable_ptr> 
+	 */
 	virtual const vector<Drawable_ptr> getDrawable()
 	{
 		vector<Drawable_ptr> out(entityManager_.getEntities().size());
