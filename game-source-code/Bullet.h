8a35cecbe804128b8af9cccfa034f45d3dbdd4a6
shivun-wits <80011093+shivun-wits@users.noreply.github.com>
2021-10-27 11:08:52 +0200

    added: Doxygen comments for Bullet Game Entity

--- game-source-code/Bullet.h
+++ game-source-code/Bullet.h
@@ -5,14 +5,33 @@
 
 using Texture_ptr = shared_ptr<sf::Texture>;
 
+/**
+ * @brief Bullet Game Entity
+ *
+ * Bullets are fired from the Ship and could potentially collide with enemies.
+ * Also known as 'Lasers'
+ */
 class Bullet : public GameEntity
 {
 public:
+	/**
+	 * @brief Construct a new Bullet object
+	 *
+	 */
 	Bullet();
 
 	void tick(const sf::Time &time);
+
+	/**
+	 * @brief Stores the minimum Y position of the bullet
+	 *
+	 */
 	constexpr static const float MIN_Y = -7.0f;
 
+	/**
+	 * @brief Stores the Velocity in pixels/second at which the bullet travels
+	 *
+	 */
 	static const sf::Vector2f VELOCITY;
 
 	bool handleCollision(entityType type, sf::FloatRect collisionRect, const shared_ptr<GameEntity> other);
