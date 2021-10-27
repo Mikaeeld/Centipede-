f9583dd52a5e7ed8e9131283ca1f722445c00530
mikaeeld <41543031+Mikaeeld@users.noreply.github.com>
2021-10-27 16:34:29 +0200

    Comments for Playstate and ResourcePath

--- game-source-code/PlayState.h
+++ game-source-code/PlayState.h
@@ -2,13 +2,41 @@
 #include "State.h"
 #include "Centipede.h"
 #include "Random.h"
+/**
+ * @brief Gameplay state
+ * 
+ * The main gameplay loop
+ * Spawns the required gameplay elements
+ * Ends the game when end game conditions are met
+ * 
+ */
 class PlayState : public State
 {
 public:
+	/**
+	 * @brief Construct a new Play State object
+	 * 
+	 */
 	PlayState();
 
+	/**
+	 * @brief Get all drawable objects contained in the state
+	 * 
+	 * @return const vector<Drawable_ptr> 
+	 */
 	const vector<Drawable_ptr> getDrawable();
+
+	/**
+	 * @brief Updates gameplay loop and entities therein
+	 * 
+	 * @param time 
+	 */
 	void update(const sf::Time &time);
+	
+	/**
+	 * @brief whether the state should be deleted. i.e. is the game over
+	 * 
+	 */
 	bool toDelete_ = false;
 
 private:
